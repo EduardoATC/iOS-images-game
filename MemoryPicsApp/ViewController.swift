a7c495956054f2313809ae26132e0ff036b8cfab
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2019-11-29 16:36:54 +0100

    Organization and comments

--- MemoryPicsApp/ViewController.swift
+++ MemoryPicsApp/ViewController.swift
@@ -34,9 +34,9 @@ class ViewController: UIViewController {
      timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(showRandomImages), userInfo: nil, repeats: true)
     }
 
-    //This function chages the imageView's images. Get a random number then
-    // use this number to select an image from the array of images in the
-    //class MemoryGame using the number as an index. Trigger the segue.
+    /*This function chages the imageView's images. Get a random number, then
+     use this number to select an image from the array of images in the
+    class MemoryGame using the number as an index. Trigger the segue.*/
     @objc func showRandomImages () {
        
         let randomNumberInGame:Int = getRandomNumber()
@@ -49,7 +49,7 @@ class ViewController: UIViewController {
                 timer.invalidate()
                 imageHasChanged = 0
             
-    // a 1 second delay between the last picture showed and the segue start.
+    //  One second delay between the last picture showed and the segue start.
             let seconds = 1.0
                 DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
                     
@@ -57,7 +57,7 @@ class ViewController: UIViewController {
                 }
             }
     }
-        //it returns a random number not equal than the previous one
+        //It returns a random number not equal than the previous one
     func getRandomNumber () ->Int {
        
         repeat {
@@ -88,13 +88,15 @@ class ViewController: UIViewController {
 
 }
 
+//Comments for my teacher.
+
 /*Para el desarollo se esta aplicación se ha utilizado en lenguaje de programación SWIFT
 desarollado por la empresa Apple, inc. Swift es un lenguaje multiparadigma enfocado en el
 desarollo de aplicaciones para dispositivios iOS y macOS. Junto a SWIFT se ha utilizado
  también, el entorno de desarollo Xcode, este contiene un conjunto de herramientas específicas, para el desarollo de aplicaciones para iOS, macOS, watchOS y tvOS.
  
  Aunque esta aplicación se podria haber desarollado utilizando el lenguaje pregramación
- objective C, se ha optado por SWIFT por su simplicidad y porque apple aconseja la utilización de este software para las nuevas aplicaciones, dado que objective C está destinado a ser sustituido por SWIFT.
+objective C, se ha optado por SWIFT por su simplicidad y porque apple aconseja la utilización de este software para las nuevas aplicaciones, dado que objective C está destinado a ser sustituido por SWIFT.
  Xcode es un entorno de desarollo cuyo propósito es el desarollo de aplicaciones para profuctos de Apple, cuenta con muchas herramientas que facilitan el trabajo, como emulador, conexión directa a repositorios, creación de archivos/clases con código útil ya escrito (ahorrandonos de esta forma tiempo), en  definitiva utilizar otro entorno de desarollo: VScode,Android Studio, procesador de texto, sería una tontería y una pérdida de tiempo dado que no cuentan con ninguna de estas características.
  
  
